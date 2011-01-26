443c3cb9a1bbe6c6cabef621c7faaf7f1d8f0416
Leigh Honeywell <leigh@hypatia.ca>
2011-01-26 09:28:12 +0000

    fussed with some path names, ignored settings.py

--- shortener/models.py
+++ shortener/models.py
@@ -5,7 +5,7 @@ from django.conf import settings
 #from django.contrib.auth.models import User
 from django import forms
 
-from urlweb.shortener.baseconv import base62
+from shortener.baseconv import base62
 
 class Link(models.Model):
     """
